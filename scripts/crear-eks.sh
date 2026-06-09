4e3376b32999c3022b691e3ab6d629ef862b5d2b
Quiti-1556 <br.quitian@duocuc.cl>
2026-06-08 21:29:01 -0400

    Agregar infraestructura EKS

---
+++ scripts/crear-eks.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+set -e
+
+REGION="us-east-1"
+CLUSTER_NAME="tienda-perritos-eks"
+NODEGROUP_NAME="tienda-node-eks"
+
+echo "====================================="
+echo " CREANDO EKS "
+echo "====================================="
+
+# Buscar VPC
+
+VPC_ID=$(aws ec2 describe-vpcs \
+ --filters "Name=tag:Name,Values=red-lab-vpc" \
+ --query "Vpcs[0].VpcId" \
+ --output text)
+
+echo "VPC: $VPC_ID"
+
+# Buscar subnets
+
+SUBNETS=$(aws ec2 describe-subnets \
+ --filters "Name=vpc-id,Values=$VPC_ID" \
+ --query "Subnets[*].SubnetId" \
+ --output text)
+
+echo "SUBNETS: $SUBNETS"
+
+# Security Group
+
+SG_ID=$(aws ec2 describe-security-groups \
+ --filters "Name=group-name,Values=SG-EKS" \
+ --query "SecurityGroups[0].GroupId" \
+ --output text)
+
+if [ "$SG_ID" == "None" ] || [ -z "$SG_ID" ]; then
+
+ SG_ID=$(aws ec2 create-security-group \
+  --group-name SG-EKS \
+  --description "EKS Security Group" \
+  --vpc-id $VPC_ID \
+  --query 'GroupId' \
+  --output text)
+
+ aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID \
+  --protocol -1 \
+  --cidr 0.0.0.0/0
+
+fi
+
+echo "SG: $SG_ID"
+
+#######################################
+# EKS CLUSTER
+#######################################
+
+STATUS=$(aws eks list-clusters \
+ --query "clusters[?@=='$CLUSTER_NAME']" \
+ --output text)
+
+if [ -z "$STATUS" ]; then
+
+ echo "Creando cluster..."
+
+ aws eks create-cluster \
+  --name $CLUSTER_NAME \
+  --region $REGION \
+  --role-arn arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):role/c215910a5457253l15425713t1w836319-LabEksClusterRole-xQqEEc06l0Sg \
+  --resources-vpc-config subnetIds=$(echo $SUBNETS | tr ' ' ','),securityGroupIds=$SG_ID,endpointPublicAccess=true,endpointPrivateAccess=true
+
+ echo "Esperando cluster ACTIVE..."
+
+ aws eks wait cluster-active \
+  --name $CLUSTER_NAME
+
+fi
+
+#######################################
+# NODE GROUP
+#######################################
+
+NODE_EXISTS=$(aws eks list-nodegroups \
+ --cluster-name $CLUSTER_NAME \
+ --query "nodegroups[?@=='$NODEGROUP_NAME']" \
+ --output text)
+
+if [ -z "$NODE_EXISTS" ]; then
+
+ echo "Creando Node Group..."
+
+ aws eks create-nodegroup \
+  --cluster-name $CLUSTER_NAME \
+  --nodegroup-name $NODEGROUP_NAME \
+  --node-role arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):role/c215910a5457253l15425713t1w836319511-LabEksNodeRole-TdrIlZuTEIhP \
+  --subnets $SUBNETS \
+  --scaling-config minSize=1,maxSize=3,desiredSize=1 \
+  --instance-types t3.large \
+  --capacity-type SPOT
+
+ echo "Esperando Node Group ACTIVE..."
+
+ aws eks wait nodegroup-active \
+  --cluster-name $CLUSTER_NAME \
+  --nodegroup-name $NODEGROUP_NAME
+
+fi
+
+#######################################
+# TAGS PARA LOAD BALANCER
+#######################################
+
+for subnet in $SUBNETS
+do
+
+ aws ec2 create-tags \
+  --resources $subnet \
+  --tags \
+  Key=kubernetes.io/cluster/$CLUSTER_NAME,Value=shared \
+  Key=kubernetes.io/role/elb,Value=1
+
+done
+
+echo "====================================="
+echo "EKS LISTO"
+echo "====================================="
+echo "CLUSTER: $CLUSTER_NAME"
+echo "NODEGROUP: $NODEGROUP_NAME"
